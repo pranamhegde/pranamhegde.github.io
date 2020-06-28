bce6144115f86d20156cc1b4c72f4c94dfd94c73
Pranam Hegde <58186996+pranamhegde@users.noreply.github.com>
2020-06-28 16:36:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function ToggleHamburgerIcon(){
+    var i = document.getElementById("horizontal-list")
+
+    if(i.style.display === "block"){
+        i.style.display = "none"
+    }else{
+        i.style.display = "block"
+    }
+
+}
